2ffc247c4bcf7eeca816900db15c51adc0483ff6
Ashitathukral <ashitathukra123@gmail.com>
2024-02-13 09:52:49 -0500

    changed code

---
+++ app.js
@@ -0,0 +1,20 @@
+// app.js
+const express = require('express');
+
+const app = express();
+const SERVER_PORT = process.env.PORT || 3000;
+
+
+// Serve an HTML homepage
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/product', (req, res) => {
+    res.sendFile(__dirname + '/views/product.html');
+  });
+
+app.listen(SERVER_PORT, () => {
+    console.log(`Server listening on port ${SERVER_PORT}`);
+    
+});
